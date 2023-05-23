4e8121b892e3bfc479859307478bd804d4e8db28
Georg Lippitsch <georg@okrasolar.com>
2023-05-24 00:01:01 +0100

    Add +CIPSTART command

--- example/wisun.c
+++ example/wisun.c
@@ -10,30 +10,57 @@
 /* helper variable used to exit demo code */
 static bool quit_flag;
 
+static char connection_type[4];
+static char connection_ip[46];
+static uint16_t connection_port;
+
 static int cgmm_run(const struct cat_command *cmd)
 {
-        printf("CC1352P7\n");
-        return CAT_RETURN_STATE_DATA_OK;
+    //TODO: Also print version?
+    printf("CC1352P7\n");
+    return CAT_RETURN_STATE_DATA_OK;
 }
 
-static int cipstart_run(const struct cat_command *cmd)
+static struct cat_variable cipstart_vars[] = {
+        {
+                .type = CAT_VAR_BUF_STRING,
+                .data = connection_type,
+                .data_size = sizeof(connection_type),
+                .name = "type"
+        },
+        {
+                .type = CAT_VAR_BUF_STRING,
+                .data = connection_ip,
+                .data_size = sizeof(connection_ip),
+                .name = "remote IP"
+        },
+        {
+                .type = CAT_VAR_INT_DEC,
+                .data = &connection_port,
+                .data_size = sizeof(connection_port),
+                .name = "remote port"
+        }
+};
+
+static int cipstart_run(const struct cat_command *cmd, const uint8_t *data, const size_t data_size, const size_t args_num)
 {
-        return CAT_RETURN_STATE_DATA_NEXT;
+    printf("Type: %s, IP: %s, port: %d\n", connection_type, connection_ip, connection_port);
+    return CAT_RETURN_STATE_DATA_OK;
 }
 
 static int cipclose_run(const struct cat_command *cmd)
 {
-        return CAT_RETURN_STATE_ERROR;
+    return CAT_RETURN_STATE_ERROR;
 }
 
 static int cipsend_run(const struct cat_command *cmd)
 {
-        return 0;
+    return 0;
 }
 
 static int ciprecvdata_run(const struct cat_command *cmd)
 {
-        return 0;
+    return 0;
 }
 
 /* declaring commands array */
@@ -46,7 +73,10 @@ static struct cat_command cmds[] = {
         {
                 .name = "+CIPSTART",
                 .description = "Connect TCP",
-                .run = cipstart_run,
+                .write = cipstart_run,
+                .var = cipstart_vars,
+                .var_num = sizeof(cipstart_vars) / sizeof(cipstart_vars[0]),
+                .need_all_vars = true
         },
         {
                 .name = "+CIPCLOSE",
